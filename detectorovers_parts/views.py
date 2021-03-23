404ea43c9aafad732806021ad599ca6ab15f335c
Rhys Horner <rhys@ronallum.com>
2021-03-23 17:14:28 +1100

    trying to add another view and template

--- detectorovers_parts/views.py
+++ detectorovers_parts/views.py
@@ -4,11 +4,7 @@ from detectorovers_parts.models import Part
 # Create your views here.
 
 def detectorover_index(request):
-    parts = Part.objects.all()
-    context = {
-        'parts':parts
-    }
-    return render(request, 'detectorover_index.html', context)
+    return render(request, 'detectorover_index.html')
 
 def parts_and_sensor_list(request):
     parts = Part.objects.all()
@@ -16,4 +12,17 @@ def parts_and_sensor_list(request):
         'parts':parts
     }
     return render(request, 'parts_and_sensor_list.html', context)
+
+# need to fix this
+def part_supplier(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
 

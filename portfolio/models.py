7d2dba21b09dd04b52a9a60d441d4fbf106483f0
rhyshorner <rhyshorner@gmail.com>
2021-05-21 22:06:18 +1000

    Sdf

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=300)
     description = models.TextField()
+    programming_language = models.CharField(max_length=50)
+    #image_url = models.URLField(max_length=400, null=True)
 
     def __str__(self):
         return 'title: {}'.format(self.title)
